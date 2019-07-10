bf51051db960e34f9b0d9d1763edc77467e645c0
Lazaro <lquin008@gmail.com>
2019-07-10 00:29:49 -0400

    toolbar updates

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BurgerLogo from "../../assets/Images/burger-logo.png";
+import styles from "./Logo.module.css";
+
+const logo = props => {
+  return (
+    <div className={styles.Logo}>
+      <img src={BurgerLogo} alt="myBurger" />
+    </div>
+  );
+};
+
+export default logo;
