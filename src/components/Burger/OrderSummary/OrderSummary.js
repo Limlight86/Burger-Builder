99211e72779deb3a217ebff7afffe20f992d31a9
Lazaro <lquin008@gmail.com>
2019-07-14 23:55:21 -0400

    added perfomance updates

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,42 @@
 import React from "react";
-import Button from '../../UI/Button/Button'
+import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey,i) => {
-    return (
-      <li key={igKey + i}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span> :
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends React.Component {
+
+  componentWillUpdate(){
+    console.log('[OrderSummary] WillUpdate')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey, i) => {
+        return (
+          <li key={igKey + i}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span> :
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
 
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A Delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total: ${props.totalPrice}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.modalClosed}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A Delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total: ${this.props.totalPrice}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.modalClosed}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,31 @@
 import React from "react";
 import styles from "./Modal.module.css";
-import Backdrop from '../Backdrop/Backdrop'
+import Backdrop from "../Backdrop/Backdrop";
 
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+  componentWillUpdate() {
+    console.log("[Modal] WillUpdate");
+  }
 
-const modal = props => {
-  return (
-    <React.Fragment>
-      <Backdrop show={props.show} clicked={props.modalClosed}/>
-      <div
-        className={styles.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </React.Fragment>
-  );
-};
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
